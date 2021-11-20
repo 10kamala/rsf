db9b9865f80fd15dd75a1b1ae84cbb9c591ac63b
10kamala <93544972+10kamala@users.noreply.github.com>
2021-11-20 13:25:07 +0100

    Create Acceuil.php

---
+++ Acceuil.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html"; charset="utf-8">
+
+<title>Missions de l'association RSF-RDC</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<link rel="stylesheet" type="text/css" href="style.css" />
+ <link rel="shortcut icon" href="images/logo.jpg">
+  
+</head>
+  <body>
+<div id="main_container">
+<div id="headerMain">
+      
+  <span style="color:white ; font-family:arial; font-size:22px;"> PRESENTATION DE  RECHERCHE SANS FRONTIERE/RESEACH WITHOUT BORDER </span> 
+  
+  <BR/>     
+   <div id="logoW"> <a href="index.php"><img src="IMAGES/image_perso.gif" width="70" height="70" alt="" border="0" /></a> </div>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+
+    <img src="IMAGES/IS.GIF" width="70" height="70" alt="" border="0" /> 
+  
+    
+   
+  </div>
+
+  <div id="header">
+   
+    <div class="banner_adds"></div>
+    <div class="menu">
+        <ul>
+	      <li><a href="">Acceuil</a></li>
+        <li><a href="">Nos missions et objectifs
+        </a>
+        <ul>
+        <li><a href="mission.php">Missions </a></li>
+        <li><a href="vision.php">Objectifs et autres</a></li>
+        
+          <li><a href="conduite.php">Bonnes conduites</a></li>
+          <li><a href="politique.php">Politique de fraude</a></li>
+        </ul>
+        </li>
+
+        <li><a href="frmAdherant.php">S'Inscrire en ligne</a></li>
+          <!--[if lte IE 6]></td></tr></table></a><![endif]-->
+       
+        <li><a href="Commentaire.php">Commentaires</a></li>
+         <li><a href="Paiement.php">Paiement</a></li>
+
+        <li><a href="">Galérie 
+        </a>
+        <ul>
+          <li><a href="addGallery">Poster l'image</a></li>
+          <li><a href="gallery.php">Afficher la galerie</a></li>
+        </ul>
+        </li>
+        
+   </div>
+    </div>  
+    <div id="headerT">
+    </div>
+  <div id="main_content">
+    <div class="column1">
+    
+      <div class="left_box">
+        <div class="top_left_box"> </div>
+        <div class="center_left_box">
+          <div class="box_title"><span>News</span>Letter</div>
+          <div class="form">
+       Entrez sur vous plait votre Adresse E-Mail pour s'inscrire au newsletter.
+            <div class="form_row">
+              <label class="left">Email: </label>
+              <input type="text" class="form_input" />
+            </div>
+            <div style="float:right; padding:10px 25px 0 0;">
+           <input type="image" src="images/join.gif" />  
+            </div>
+          </div>
+        </div>
+    
+        <div class="bottom_left_box"> </div>
+      </div>
+      <div class="left_box">
+        <div class="top_left_box"> </div>
+        <div class="center_left_box">
+          <div class="box_title"><span>Contact</span> information:</div>
+          <div class="form">
+            <div class="form_row"> <img src="images/contact_envelope.gif" width="50" height="47" border="0" class="img_right" alt="" />
+              <div class="contact_information"> Email: rsfrdc2015@gmail.com<br />
+        
+                   Airtel   : +243-972 044 570<br />
+                
+                   Vodacom  : +243-824 787 878 <br />
+
+                   Orange-RDC : +243-848 400 338 <br /> <br />
+
+                   <span><strong>Nos comptes bancaires :</strong></span> <br />
+                   
+                   
+                   <ul>
+                    <li>TMB Numéro de compte: 1272-7401426-00-91</li>  <br />
+                    <li>EQUITY BANQUE Numéro de compte: 028200000274862</li>
+                  </ul>
+                <br />
+                <span>www.rsfrdc.com</span> 
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="bottom_left_box"> </div>
+      </div>
+    </div>
+
+<div id="header">
+    
+</div>
+   <img src="PhotoAdherant/Tulips.jpg" class="tailleimage"  WIDTH="690"  height="345" />
+</body>
+</html>
